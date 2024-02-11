60ad8d1e8ebdaa0aa16f759649c97a902875a5c7
Phenzic <juliuayoola5@gmail.com>
2024-02-11 23:27:51 +0100

    added more endpoints

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from email.mime.application import MIMEApplication
 from pymongo import MongoClient
 from bson import ObjectId
 import uuid
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+from bson.json_util import dumps
 
 
 app = Flask(__name__)
@@ -25,13 +27,19 @@ db = client.db
 court_data = db['court_data']
 affidavit_data = db['affidavit_data']
 
+
+# @dev signs up a court details and updates it, only if the creator calls the request,
+# @dev returns the court_id of the court that was signed up
 @app.route('/court-signup', methods=['POST'])
+@jwt_required()
 def court_signup():
+    current_user = get_jwt_identity()
     """
     Endpoint for court signup.
     """
     try:
         data = request.json["data"]
+        data["creator"] = current_user
         existing_user = court_data.find_one({'name': data['name']})
         if existing_user:
             court_data.update_one({'name': data['name']}, {'$set': data})
@@ -70,31 +78,21 @@ def index():
     """
     return 'Welcome to the e-affidavit server!'
 
-@app.route('/get_id', methods=['POST'])
-def get_id():
+
+@app.route('/get_court_details', methods=['POST'])
+def get_court_details():
     # Endpoint to get app ID.
     # TODO: Implement the logic to get the app ID
     try:
         app_id = request.json["app_id"]
-        obj = court_data.find_one({'_id': ObjectId(app_id)})
         existing_user = court_data.find_one({'_id': ObjectId(app_id)})
-        if existing_user:
+        if existing_user:    
+            existing_user.pop('_id')
+            existing_user["_id"] = app_id  # Exclude the '_id' field from the response
             response_message = {
                 'status': 'success',
                 'message': 'App ID found',
-                'data': {
-                    '_id': str(existing_user['_id']),  # Convert ObjectId to string
-                    'name': existing_user['name'],
-                    'color': existing_user['color'],
-                    'verification': existing_user['verification'],
-                    'user_information': existing_user['user_information'],
-                    'on_verification': existing_user['on_verification'],
-                    'redirect_url': existing_user['redirect_url'],
-                    'courtLogo': existing_user['courtLogo'],
-                    'courtName': existing_user['courtName'],
-                    'courtAddress': existing_user['courtAddress'],
-                    'commissionerForOathSignature': existing_user['commissionerForOathSignature']
-                }
+                'data': existing_user
             }
             return jsonify(response_message), 200
         else:
@@ -108,37 +106,8 @@ def get_id():
 
 
 
-# @app.route('/update_data', methods=['POST'])
-# def update_data():
-#     """
-#     Endpoint for court signup.
-#     """
-
-#     try:
-#         data = request.json["data"]
-#         existing_user = court_data.find_one({'username': data['username']})
-#         if existing_user:
-#             raise Exception('User with this username already exists')
-
-#         inserted_data = court_data.insert_one(data)
-
-#         response_message = {
-#             'status': 'success',
-#             'message': 'Data added to MongoDB collection',
-#             'inserted_id': str(inserted_data)
-#         }
-
-        return jsonify(response_message), 200
-
-    except Exception as e:
-        error_message = {
-            'status': 'error',
-            'message': str(e)
-        }
-        return jsonify(error_message), 500
-
-@app.route('/keep_affidavit', methods=['POST'])
-def keep_affidavit():
+@app.route('/save_affidavit', methods=['POST'])
+def save_affidavit():
     try:
         data = request.json["data"]
         existing_data = affidavit_data.find_one({'id': data['id']})
@@ -150,7 +119,6 @@ def keep_affidavit():
         inserted_data = affidavit_data.insert_one(data)
         new_data = affidavit_data.find_one({'id': data['id']})
 
-
         response_message = {
             'status': 'success',
             'message': 'Data added to MongoDB collection',
@@ -160,6 +128,40 @@ def keep_affidavit():
 
         return jsonify(response_message), 200
 
+    except Exception as e:
+        error_message = {
+            'status': 'error',
+            'message': str(e)
+        }
+        return jsonify(error_message), 500
+    
+
+
+@app.route('/getAllCourtAffidavit', methods=['POST'])
+def get_court_affidavit():
+    court_id = request.json["court_id"]
+    try:
+        result = affidavit_data.find({'court_id': court_id})
+        documents = [doc for doc in result]
+        for doc in documents:
+            # doc.pop('_id', None)  # Remove the "_id" variable
+            doc['_id'] = str(doc['_id'])  # Convert ObjectId to string
+        return dumps(documents), 200
+    except Exception as e:
+        error_message = {
+            'status': 'error',
+            'message': str(e)
+        }
+        return jsonify(error_message), 500
+
+@app.route('/getAllCourtAffidavitId', methods=['POST'])
+def get_affidavit():
+    court_id = request.json["court_id"]
+    try:
+        results = affidavit_data.find({'court_id': court_id}, {'_id': 1})
+        # Convert _id to string
+        results = [{'_id': str(res['_id'])} for res in results]
+        return jsonify(results), 200
     except Exception as e:
         error_message = {
             'status': 'error',
